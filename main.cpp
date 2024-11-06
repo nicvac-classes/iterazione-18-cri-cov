0cc3cbc14e8435fa745c36ea8d6b7cd8ed696256
cri-cov <cristian.covato@marconibari.edu.it>
2024-11-06 17:22:35 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
-#include <iostream>
-using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int i;
+    float media,livello,somma;
+    i=1;
+    somma=0;
+    while(i<=7){
+        cout<<"di quanto si alza l'acqua"<<endl;
+        cin>>livello;
+        if(livello>=10){
+            cout<<"nel giorno"<<i<<"l'acqua è alta"<<endl;
+            }
+            somma=somma+livello;
+            media=somma/7;
+            i=i+1;
+            }
+            if(media>0){
+                cout<<"l'acqua è alta"<<endl;
+               }else{
+            cout<<"l'acqua è bassa"<<endl;
+           }
+           cout<<"la media dei valori è"<<media<<endl;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    
+}
